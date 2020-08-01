5d63401aaf0f1eb227c51f279a30a8375cb87ad9
markwafik <markwafik@hotmail.com>
2020-08-01 18:36:59 +0300

    website

---
+++ js/index.js
@@ -0,0 +1,39 @@
+var nav = document.querySelector('nav')
+
+
+window.onscroll = function()
+{
+
+    if(document.body.scrollTop > 100)
+    {
+        nav.classList.remove('bg-dark')
+        nav.classList.add('bg-gray')
+    
+    }else if(document.body.scrollTop < 100)
+    {
+        nav.classList.remove('bg-gray')
+        nav.classList.add('bg-dark')
+
+    }
+}
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
